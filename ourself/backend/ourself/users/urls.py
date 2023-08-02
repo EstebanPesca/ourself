89f76aa2e6b6bae5a21181ed07200f8b637bf0e5
EstebanPesca <benyi022011@gmail.com>
2023-08-02 18:20:37 -0500

    c - register user

--- ourself/backend/ourself/users/urls.py
+++ ourself/backend/ourself/users/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from .api.views import UserLoginView
+from .api.routers import router
 
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name='user-login'),
+    path('', include(router.urls)),
 ]
